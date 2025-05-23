2f363a40ddb2bd9fdd13c04870d12983571ab033
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-23 04:23:48 -0500

    add chapter 4 script

---
+++ var/lib/lfstage/profiles/x86_64-glibc-tox-stage2/scripts/04-chapter4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 4.2. Creating a Limited Directory Layout in the LFS Filesystem
+mkdir -pv "$LFS"/{etc,var,tools} "$LFS"/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+    ln -sv usr/$i "$LFS/$i"
+done
+
+case $(uname -m) in
+    x86_64) mkdir -pv "$LFS/lib64" ;;
+esac
+
+# NOTE: The LFS user is skipped
