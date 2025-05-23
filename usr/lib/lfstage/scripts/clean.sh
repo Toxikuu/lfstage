80b3a2057c5e5b4485f8e9dbbe2758e245d7a1ac
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-23 04:24:41 -0500

    add cleaning script

---
+++ usr/lib/lfstage/scripts/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to clean up any artifacts of a previous LFStage build
+
+# Sanity check
+if [[ "$LFS" != "/var/lib/lfstage/mount" ]]; then 
+    die "\$LFS isn't properly set: $LFS" 33
+fi
+
+# Unmount anything from a previous lfstage build
+if mount | grep "^$LFS" -q; then
+    umount -vR "$LFS"
+fi
+
+# Remove any stale files
+rm -rf "${LFS:?}/"*
